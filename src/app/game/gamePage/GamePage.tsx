578591ff7f388d23a2ef8dd05b6b4d8a3b839a1a
Marek Kustosz <markust71@hotmail.com>
2021-05-20 23:05:18 +0200

    flip

--- src/app/game/gamePage/GamePage.tsx
+++ src/app/game/gamePage/GamePage.tsx
@@ -4,29 +4,38 @@ import { useSelector } from 'react-redux';
 import { GameState } from 'reducers/game/gameReducer.types';
 
 export const GamePage = () => {
-  const [flip, setFlip] = useState(false);
+  const [cardFlip, setCardFlip] = useState<boolean[]>([]);
 
   const cards = useSelector<GameState, GameState['cards']>((state) => state.cards) || [];
 
+  const handleClick = (index: number) => {
+    const newCardFlip = [...cardFlip];
+    newCardFlip[index] = !newCardFlip[index];
+    setCardFlip([...newCardFlip]);
+  };
+
   useEffect(() => {
-    setFlip(false);
+    const flips = new Array(12);
+    flips.fill(false);
+    setCardFlip([...flips]);
   }, []);
 
   if (!cards) return null;
 
   return (
-    <div className="wrapper inner-wrapper" onClick={() => setFlip((prevState) => !prevState)}>
+    <div className="wrapper inner-wrapper">
       <div className="table">
         {cards.length > 0 &&
           cards.map((card, index) => (
-            <div key={`${card.value}-${index}`} className={`card-grid-box ${flip && index === 5 ? 'flip' : ''}`}>
+            <div key={`${card.value}-${index}`} className={`card-grid-box ${cardFlip[index] ? 'flip' : 'no-flip'}`}>
               <div
                 style={{
                   backgroundImage: `url('${card.images.png}')`,
                 }}
-                className="card-box"
+                className="card-box flip"
+                onClick={() => handleClick(index)}
               />
-              <div className="card-box-back" />
+              <div className="card-box-back no-flip" onClick={() => handleClick(index)} />
             </div>
           ))}
       </div>
